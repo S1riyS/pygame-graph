4fe027aa905055f25704c16f82967337e242515e
S1riyS <kirill.ankudinov.94@mail.ru>
2022-09-25 12:20:23 +0500

    FEATURE: math function moved to separated function

--- run.py
+++ run.py
@@ -2,6 +2,11 @@ import math
 
 from main import PygameGraph
 
+
+def func(x: float) -> float:
+    return math.sin(x)
+
+
 if __name__ == '__main__':
-    app = PygameGraph(function=math.cos, scale=40)
+    app = PygameGraph(function=func, scale=40)
     app.run()
