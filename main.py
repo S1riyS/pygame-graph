2c0b2295d09f61a2f85e68f4c435ccf57ee8cc17
S1riyS <kirill.ankudinov.94@mail.ru>
2022-09-26 14:03:25 +0500

    FEATURE: several functions can be displayed

--- main.py
+++ main.py
@@ -6,15 +6,36 @@ from pygame.math import Vector2
 from config import Config
 
 
+class Function(t.NamedTuple):
+    function: t.Callable
+    left_x: t.Optional[float] = None
+    right_x: t.Optional[float] = None
+
+
 class PygameGraph:
-    def __init__(self, function: t.Callable, scale: int):
+    def __init__(
+            self,
+            functions: t.Iterable[Function],
+            scale: int,
+    ):
         pygame.init()
-        self.function = function
+        self.functions = functions
         self.graph_scale = scale
 
         self.WIDTH = Config.WINDOW_WIDTH
         self.HEIGHT = Config.WINDOW_HEIGHT
         self.GRAPH_CENTER = Vector2(self.WIDTH // 2, self.HEIGHT // 2)
+        self.COLORS = (
+            pygame.Color(255, 0, 0),
+            pygame.Color(0, 255, 0),
+            pygame.Color(0, 0, 255),
+            pygame.Color(25, 25, 25),
+            pygame.Color(255, 0, 255),
+            pygame.Color(0, 255, 255),
+            pygame.Color(236, 123, 45),
+            pygame.Color(35, 35, 35),
+            pygame.Color(123, 123, 123),
+        )
 
         self.screen = pygame.display.set_mode((self.WIDTH, self.HEIGHT))
         self.running = True
@@ -37,7 +58,8 @@ class PygameGraph:
 
             self.__normalize_graph_scale()
             self.__draw_coordinate_plane()
-            self.__draw_function()
+            for function, color in zip(self.functions, self.COLORS):
+                self.__draw_function(function, color)
 
             pygame.display.flip()
 
@@ -87,20 +109,25 @@ class PygameGraph:
                 (self.GRAPH_CENTER.x + Config.DIVIDING_STRIPES_WIDTH, strip_y_coordinate),
             )
 
-    def __draw_function(self) -> None:
+    def __draw_function(self, function: Function, color: pygame.Color) -> None:
         """Draws graph of given function"""
         previous_dot_position = None
+        if function.left_x and function.right_x:
+            left_border = round(function.left_x * self.graph_scale)
+            right_border = round(function.right_x * self.graph_scale)
+        else:
+            half_width = ((self.WIDTH - 2 * Config.DIVIDING_STRIPES_OFFSET) // 2)
+            left_border = self.graph_scale * (half_width // self.graph_scale)
+            right_border = -self.graph_scale * (half_width // self.graph_scale)
 
-        half_width = ((self.WIDTH - 2 * Config.DIVIDING_STRIPES_OFFSET) // 2)
-        border = self.graph_scale * (half_width // self.graph_scale)
-
-        for x in range(-border, border):
+        for x in range(left_border, right_border):
             try:
-                graph_dot_offset = Vector2(x, -self.function(x / self.graph_scale) * self.graph_scale)
+                graph_dot_offset = Vector2(x, -function.function(x / self.graph_scale) * self.graph_scale)
                 current_dot_position = self.GRAPH_CENTER + graph_dot_offset
 
-                if previous_dot_position and abs(previous_dot_position.y - current_dot_position.y) < self.HEIGHT:
-                    pygame.draw.line(self.screen, Config.BLACK, previous_dot_position, current_dot_position, 1)
+                if previous_dot_position:
+                    if abs(previous_dot_position.y - current_dot_position.y) < self.HEIGHT:
+                        pygame.draw.line(self.screen, color, previous_dot_position, current_dot_position, 1)
 
                 previous_dot_position = current_dot_position
             except (ValueError, ZeroDivisionError):
--- run.py
+++ run.py
@@ -1,12 +1,17 @@
 import math
 
-from main import PygameGraph
+from main import PygameGraph, Function
 
-
-def func(x: float) -> float:
-    return math.sin(x)
-
+FUNCTIONS = (
+    Function(function=lambda x: -x ** 2, left_x=-2, right_x=2),
+    Function(function=lambda x: (x + 5) ** 2, left_x=-8, right_x=2),
+    Function(function=lambda x: -(x - 3) ** 2 + 4, left_x=0.5, right_x=5.5),
+    Function(function=lambda x: (x + 4) ** 2 - 4, left_x=-6.5, right_x=-1.5),
+    Function(function=lambda x: -(x + 2) ** 2 + 3, left_x=-4.5, right_x=0.5),
+    Function(function=lambda x: -(x - 6) ** 2, left_x=3.5, right_x=8.5),
+    Function(function=lambda x: math.cos(x))
+)
 
 if __name__ == '__main__':
-    app = PygameGraph(function=func, scale=40)
+    app = PygameGraph(functions=FUNCTIONS, scale=40)
     app.run()
